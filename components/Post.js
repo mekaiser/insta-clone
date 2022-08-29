b78a1a805a4b3099db3891b33f5bd4b576e563f4
mekaiser <kaiser331042@gmail.com>
2022-08-29 18:00:52 +0600

    updated readme.md

--- components/Post.js
+++ components/Post.js
@@ -122,6 +122,9 @@ export default function Post({ id, username, img, userImg, caption }) {
 
       {/* Caption */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className="font-bold mr-1">{username} </span>
         {caption}
       </p>
